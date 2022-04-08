35fc93aa652351488a7bb4114be5a19845e7f0c0
Maxim Shamrev <shmarev082@gmail.com>
2022-04-08 09:56:25 +0300

    Init commit

---
+++ Deadline.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlineLibrary
+{
+    public class Deadline
+    {
+        public static string UpdateTime(DateTime deadline)
+        {
+            string result;
+            TimeSpan interval = deadline.Subtract(DateTime.Now);
+            if (deadline.CompareTo(DateTime.Now) > 0)
+                result = interval.ToString(@"dd") + " days " + interval.ToString(@"hh\:mm\:ss");
+            else
+                result = "Время вышло";
+            return result;
+        }
+    }
+}
